78b8a3a74a5a26ff62773e87a55e9b46e3e7ba73
Cristiane Sebem Damo <cristiane@classcode.com.br>
2024-04-27 15:57:13 -0300

    adiciona cors

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from flask import Flask
+from flask_cors import CORS
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
@@ -32,9 +33,9 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-   
 
     db.init_app(app)
+    CORS(app)
     migrate = Migrate(app, db)
 
     api = Api(app)
